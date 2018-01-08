1ff0df226606d1ff9abd62bcd6d74ecd20d34481
wo_fpc <wo_fpc@outlook.com>
2018-01-08 21:29:37 +0800

    hex editor

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -12,7 +12,6 @@ namespace HexMap
         public Text cellLabelPrefab;
 
         public Color defaultColor = Color.white;
-        public Color touchedColor = Color.magenta;
 
         private HexCell[] cells;
         private Canvas gridCanvas;
@@ -37,31 +36,13 @@ namespace HexMap
             hexMesh.Triangulate(cells);
         }
 
-        private void Update()
-        {
-            if (Input.GetMouseButton(0))
-            {
-                HandleInput();
-            }
-        }
-
-        private void HandleInput()
-        {
-            Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(inputRay, out hit))
-            {
-                TouchCell(hit.point);
-            }
-        }
-
-        private void TouchCell(Vector3 position)
+        public void ColorCell(Vector3 position, Color color)
         {
             position = transform.InverseTransformPoint(position);
             HexCoordinates coordinates = HexCoordinates.FromPosition(position);
             int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
             HexCell cell = cells[index];
-            cell.color = touchedColor;
+            cell.color = color;
             hexMesh.Triangulate(cells);
         }
 
