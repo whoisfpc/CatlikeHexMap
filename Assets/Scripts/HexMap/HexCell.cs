028899ab1732333f8a458263414eb4c20ce7a166
whoisfpc <wo_fpc@outlook.com>
2018-03-18 18:14:56 +0800

    highlight

--- Assets/Scripts/HexMap/HexCell.cs
+++ Assets/Scripts/HexMap/HexCell.cs
@@ -365,6 +365,19 @@ namespace HexMap
             }
         }
 
+        public void DisableHighlight()
+        {
+            Image highlight = uiRect.GetChild(0).GetComponent<Image>();
+            highlight.enabled = false;
+        }
+
+        public void EnableHighlight(Color color)
+        {
+            Image highlight = uiRect.GetChild(0).GetComponent<Image>();
+            highlight.color = color;
+            highlight.enabled = true;
+        }
+
         public HexCell GetNeighbor(HexDirection direction)
         {
             return neighbors[(int)direction];
