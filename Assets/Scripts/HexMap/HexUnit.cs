a24a708cce1f4cca04d4d358cd8998b9b6ea3530
whoisfpc <wo_fpc@outlook.com>
2018-03-27 22:59:53 +0800

    animate rotation

--- Assets/Scripts/HexMap/HexUnit.cs
+++ Assets/Scripts/HexMap/HexUnit.cs
@@ -9,6 +9,7 @@ namespace HexMap
     public class HexUnit : MonoBehaviour
     {
         private const float travelSpeed = 4f;
+        private const float rotationSpeed = 180f;
 
         public static HexUnit unitPrefab;
 
@@ -63,6 +64,8 @@ namespace HexMap
         private IEnumerator TravelPath()
         {
             Vector3 a, b, c = pathToTravel[0].Position;
+            transform.localPosition = c;
+            yield return LookAt(pathToTravel[1].Position);
 
             float t = Time.deltaTime * travelSpeed;
             for (int i = 1; i < pathToTravel.Count; i++)
@@ -73,6 +76,9 @@ namespace HexMap
                 for (; t < 1f; t += Time.deltaTime * travelSpeed)
                 {
                     transform.localPosition = Bezier.GetPoint(a, b, c, t);
+                    Vector3 d = Bezier.GetFirstDerivative(a, b, c, t);
+                    d.y = 0f;
+                    transform.localRotation = Quaternion.LookRotation(d);
                     yield return null;
                 }
                 t -= 1f;
@@ -84,10 +90,37 @@ namespace HexMap
             for (; t < 1f; t += Time.deltaTime * travelSpeed)
             {
                 transform.localPosition = Bezier.GetPoint(a, b, c, t);
+                Vector3 d = Bezier.GetFirstDerivative(a, b, c, t);
+                d.y = 0f;
+                transform.localRotation = Quaternion.LookRotation(d);
                 yield return null;
             }
 
             transform.localPosition = location.Position;
+            orientation = transform.localRotation.eulerAngles.y;
+            ListPool<HexCell>.Add(pathToTravel);
+            pathToTravel = null;
+        }
+
+        IEnumerator LookAt(Vector3 point)
+        {
+            point.y = transform.localPosition.y;
+            Quaternion fromRotation = transform.localRotation;
+            Quaternion toRotation = Quaternion.LookRotation(point - transform.localPosition);
+            float angle = Quaternion.Angle(fromRotation, toRotation);
+            float speed = rotationSpeed / angle;
+
+            if (angle > 0)
+            {
+                for (float t = Time.deltaTime * speed; t < 1f; t += Time.deltaTime * speed)
+                {
+                    transform.localRotation = Quaternion.Slerp(fromRotation, toRotation, t);
+                    yield return null;
+                }
+
+                transform.LookAt(point);
+                orientation = transform.localRotation.eulerAngles.y;
+            }
         }
 
         public void Die()
@@ -119,35 +152,6 @@ namespace HexMap
             if (location)
             {
                 transform.localPosition = location.Position;
-            }
-        }
-
-        private void OnDrawGizmos()
-        {
-            if (pathToTravel == null || pathToTravel.Count == 0)
-            {
-                return;
-            }
-
-            Vector3 a, b, c = pathToTravel[0].Position;
-
-            for (int i = 1; i < pathToTravel.Count; i++)
-            {
-                a = c;
-                b = pathToTravel[i - 1].Position;
-                c = (b + pathToTravel[i].Position) * 0.5f;
-                for (float t = 0f; t < 1f; t += 0.1f)
-                {
-                    Gizmos.DrawSphere(Bezier.GetPoint(a, b, c, t), 2f);
-                }
-            }
-
-            a = c;
-            b = pathToTravel[pathToTravel.Count - 1].Position;
-            c = b;
-            for (float t = 0f; t < 1f; t += 0.1f)
-            {
-                Gizmos.DrawSphere(Bezier.GetPoint(a, b, c, t), 2f);
             }
         }
     }
--- Assets/Scripts/HexMap/Util/Bezier.cs
+++ Assets/Scripts/HexMap/Util/Bezier.cs
@@ -11,6 +11,12 @@ namespace HexMap.Util
             return oneMinusT * oneMinusT * p0 + 2f * oneMinusT * t * p1 + t * t * p2;
         }
 
+        public static Vector3 GetFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+        {
+            t = Mathf.Clamp01(t);
+            return 2f * ((1f - t) * (p1 - p0) + t * (p2 - p1));
+        }
+
         public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
         {
             t = Mathf.Clamp01(t);
